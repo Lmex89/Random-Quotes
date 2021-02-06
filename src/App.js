8c86ad91f4f8a6280270b423530bfbd203d2d507
Lmex89 <bjt.mex@gmail.com>
2021-02-06 17:43:25 -0600

    added-quote-example-as-varible

--- src/App.js
+++ src/App.js
@@ -1,62 +1,53 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+const Example = {
+  quote : "Carpe Diem",
+  author : "Anonimo" 
+}
 
 const Card = () => {
-    const [quote, setQuote] = useState('Carpe Diem ');
-    const [author, setAuthor] = useState('Anonimo ');
+  const [quote, setQuote] = useState(Example.quote);
+  const [author, setAuthor] = useState(Example.author);
 
-    function randomQuote() {
-        const data = require('./quotes.json');
-        let dataQuotes = data;
-        let rNum = Math.floor(Math.random() * dataQuotes.length);
-        let randomQuote = dataQuotes[rNum];
-        setQuote(randomQuote.quoteText);
-        setAuthor("-" + randomQuote.quoteAuthor);
+  function randomQuote() {
+    const data = require('./quotes.json'); 
+    let dataQuotes = data;
+    let rNum = Math.floor(Math.random() * dataQuotes.length);
+    let randomQuote = dataQuotes[rNum];
+    setQuote(randomQuote.quoteText);
+    setAuthor('-' + randomQuote.quoteAuthor);
+  }
 
-    }
+  const handleClick = () => {
+    randomQuote();
+  };
 
-    const handleClick = () => {
-        randomQuote();
 
-    };
-
-    return ( <
-        div className = "card"
-        id = "quote-box" >
-        <
-        div id = "text" >
-        <
-        h2 > { quote } < /h2> < /
-        div > <
-        div id = "author" >
-        <
-        p > { author } < /p> < /
-        div > <
-        div id = "buttons" >
-        <
-        div className = "social-media" >
-        <
-        /
-        div > <
-        button onClick = { handleClick }
-        id = "new-quote" >
-
-        New Quote <
-        /button> < /
-        div > <
-        /div>
-    );
+  return (
+    <div className="card" id="quote-box">
+      <div id="text">
+        <h2> {quote} </h2>{' '}
+      </div>{' '}
+      <div id="author">
+        <p> {author} </p>{' '}
+      </div>{' '}
+      <div id="buttons">
+        <div className="social-media"> </div>{' '}
+        <button onClick={handleClick} id="new-quote">
+          New Quote{' '}
+        </button>{' '}
+      </div>{' '}
+    </div>
+  );
 };
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Card / >
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
